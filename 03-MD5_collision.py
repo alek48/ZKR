4ce43749b8e5d60222ce5922ae9ad52320517a0f
JustSomeBirb <34716018+alek48@users.noreply.github.com>
2023-04-26 16:53:24 +0200

    Add files via upload

---
+++ 03-MD5_collision.py
@@ -0,0 +1,21 @@
+from hashlib import sha256
+from os import urandom
+
+iterations = 1_000_000
+collision_times = []
+for _ in range(iterations):
+    i = 0
+    bits_list = []
+    while True:
+        i += 1
+        data = urandom(128)
+        hash_head = sha256(data).hexdigest()[:3]
+        if hash_head not in bits_list:
+            bits_list.append(hash_head)
+        else:
+            collision_times.append(i)
+            break
+print("\nLiczba skrótów potrzebna do kolizji na pierwszych 12 bitach (wykonano {} iteracji):".format(iterations))
+print("minimalnie: {}".format(min(collision_times)))
+print("Średnio: {}".format(sum(collision_times)/len(collision_times)))
+print("Maksymalnie: {}".format(max(collision_times)))
